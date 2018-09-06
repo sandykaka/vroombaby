9ff8b6163f01a93d4e806834f26f642e3b35cce2
Sandesh Kakade <sandeshkakade@Sandeshs-MacBook-Pro.local>
2018-09-06 15:39:04 -0500

    added ip address

--- website1/settings.py
+++ website1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')1x1+(1p0x*=8_0lfcn8^6!*#6ri-9zavxi14@nyug%!pa5k0m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.vroombaby.com', 'vroombaby.com']
+ALLOWED_HOSTS = ['www.vroombaby.com', 'vroombaby.com', '54.88.228.55']
 # ALLOWED_HOSTS = []
 
 
