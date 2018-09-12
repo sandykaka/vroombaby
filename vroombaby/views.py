4ee461239021bc52ae4aeebdd3fa93d467706e9d
Vanessa Edirisuriya <vanessaedirisuriya@mac-34363b6cffac.attlocal.net>
2018-09-12 18:13:40 -0500

    updated google view

--- vroombaby/views.py
+++ vroombaby/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,4 +6,4 @@ def index(request):
     return render(request, 'vroombaby/index.html')
 
 def googleeb914ff572b518f7(request):
-    return render(request, 'vroombaby/googleeb914ff572b518f7.html')
+    return HttpResponse('vroombaby/googleeb914ff572b518f7.html')
