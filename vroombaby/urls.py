18b3101fcdd818b11cb933992851456a4c7b03f4
Vanessa Edirisuriya <vanessaedirisuriya@mac-34363b6cffac.attlocal.net>
2018-09-12 17:59:43 -0500

    updated path

--- vroombaby/urls.py
+++ vroombaby/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'vroombaby'
 urlpatterns = [
     path('', views.index, name='index'),
-    path(r'^googleeb914ff572b518f7\.html$', lambda r: HttpResponse("google-site-verification: googleeb914ff572b518f7.html", mimetype="text/plain")),
+    path('googleeb914ff572b518f7.html', lambda r: HttpResponse("google-site-verification: googleeb914ff572b518f7.html", mimetype="text/plain")),
 
 ]
