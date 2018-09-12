966a6db834cf6b9e52526e40107597de09dca3a8
Vanessa Edirisuriya <vanessaedirisuriya@mac-34363b6cffac.attlocal.net>
2018-09-12 17:42:30 -0500

    added google verification

--- vroombaby/urls.py
+++ vroombaby/urls.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.urls import path
 
 from . import views
@@ -5,4 +6,6 @@ from . import views
 app_name = 'vroombaby'
 urlpatterns = [
     path('', views.index, name='index'),
+    path(r'^googleeb914ff572b518f7\.html$', lambda r: HttpResponse("google-site-verification: googleeb914ff572b518f7.html", mimetype="text/plain")),
+
 ]
