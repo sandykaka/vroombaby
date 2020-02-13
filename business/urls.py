dde9027239fa3bd718c56772d902d37354562439
skakade <sk_22@apple.com>
2020-02-13 13:18:41 -0600

    added files

---
+++ business/urls.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.urls import path
+
+from . import views
+
+app_name = 'business'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
+    path('products.html',views.products, name='products'),
+    path('googleeb914ff572b518f7', views.googleeb914ff572b518f7),
+]
---
+++ business/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'business/index.html')
+
+def products(request):
+    return render(request, 'business/products.html')
+
+def googleeb914ff572b518f7(request):
+    return HttpResponse('business/googleeb914ff572b518f7.html')
