8816ff6f777f424bbc311df4c88e034bf0c0fc49
skakade <sk_22@apple.com>
2025-01-23 17:33:16 -0800

    updated allowed host

--- website1/settings.py
+++ website1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')1x1+(1p0x*=8_0lfcn8^6!*#6ri-9zavxi14@nyug%!pa5k0m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.vroombaby.com', 'vroombaby.com', '54.88.228.55', '172.26.12.62']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.vroombaby.com', 'vroombaby.com', '54.88.52.63', '172.26.12.62']
 # ALLOWED_HOSTS = []
 
 
