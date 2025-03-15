ceebd5d6b1b623865ae2df82bd0b281e4597c423
skakade <sk_22@apple.com>
2025-03-15 13:19:53 -0700

    updated callback url

--- business/urls.py
+++ business/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('delete-meeting/<int:meeting_id>/', views.delete_meeting, name='delete_meeting'),
     path('update-meeting/<int:meeting_id>/', views.update_meeting, name='update_meeting'),
     path('linkedin-login/', views.linkedin_login, name='linkedin_login'),
-    path('linkedin-callback/', views.linkedin_callback, name='linkedin_callback'),
+    path('linkedin-callback', views.linkedin_callback, name='linkedin_callback'),
     path('apple-app-site-association', views.apple_app_site_association, name='apple_app_site_association'),
     path('.well-known/apple-app-site-association', views.apple_app_site_association, name='apple_app_site_association'),
 
